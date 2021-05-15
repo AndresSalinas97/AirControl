187772acf5693a4314084f1683a82ec9c9fcb454
AndresSalinas97 <i52salia@uco.es>
2021-05-15 11:09:25 +0200

    Remove main

--- AirControl/src/i52salia/si/trabajofinal/aircontrol/AirControlView.java
+++ AirControl/src/i52salia/si/trabajofinal/aircontrol/AirControlView.java
@@ -38,7 +38,7 @@ public final class AirControlView extends javax.swing.JFrame {
 
     private void customizeComponents() {
         if (!System.getProperty("os.name").toLowerCase().contains("mac")) {
-            customizeTabs(); // Doesn't work with macOS look and feel
+            customizeTabs(); // Doesn't work well with macOS look and feel
         }
 
         this.setPreferredSize(INITIAL_SIZE);
@@ -166,44 +166,6 @@ public final class AirControlView extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AirControlView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AirControlView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AirControlView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AirControlView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AirControlView().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel homeTab;
