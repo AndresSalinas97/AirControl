6e8138e7caf22a31fd1ceb2aeb5a8bf09988cd07
AndresSalinas97 <i52salia@uco.es>
2021-05-16 19:32:20 +0200

    Remove default javadocs

--- src/i52salia/aircontrol/components/DayOfWeekSelector.java
+++ src/i52salia/aircontrol/components/DayOfWeekSelector.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package i52salia.aircontrol.components;
 
 /**
- *
- * @author andres
+ * TODO
  */
 public class DayOfWeekSelector extends javax.swing.JPanel {
 
