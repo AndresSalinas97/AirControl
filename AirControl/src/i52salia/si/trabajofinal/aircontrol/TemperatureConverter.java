28a2b6928d569e05207e261d91ab4306ef559da2
AndresSalinas97 <i52salia@uco.es>
2021-05-16 10:40:45 +0200

    Create TemperatureConverter.java

---
+++ AirControl/src/i52salia/si/trabajofinal/aircontrol/TemperatureConverter.java
@@ -0,0 +1,21 @@
+package i52salia.si.trabajofinal.aircontrol;
+
+import java.text.DecimalFormat;
+
+/**
+ * A class with methods to convert and print temperature values.
+ *
+ * @author Andrés Salinas Lima {@literal <i52salia@uco.es>}
+ */
+public final class TemperatureConverter {
+
+    /**
+     * @param celsius temperature in Celsius
+     * @return string with the temperature in Celsius with 2 decimal points
+     */
+    public final static String celsiusToCelsiusString(double celsius) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
+        return (decimalFormat.format(celsius) + " ºC");
+    }
+}
